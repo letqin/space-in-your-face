177c0ef56b3795fd8383818822003bf0683b5237
TS93 <tschram93@yahoo.com>
2021-09-07 17:22:28 -0400

    Fetch'd ISS  API with logs, added issFlexBox variable

---
+++ script.js
@@ -0,0 +1,20 @@
+// Establish variable for the location of "ISS" flexbox.
+var issFlexBox = document.body.querySelector('#issFlexBox');
+console.log(issFlexBox);
+
+// Fetch WhereIsTheISS API
+
+fetch('https://api.wheretheiss.at/v1/satellites', {
+        // method: "POST",
+        // body: {}
+    })
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+        } else {
+            console.log('Not Successful')
+        }
+        res.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
