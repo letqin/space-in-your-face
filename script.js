a0cc64e59c6b885bf6cb929ed27999156744e463
TS93 <tschram93@yahoo.com>
2021-09-08 18:56:55 -0400

    updated api whereistheiss link with TLES format

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// API Key NASA
+let key = "BSrNrFfi6oeF8CToW9wqHYUh1VIQeAAbQTgdzp5t";
+
 // Establish variable for the location of "ISS" flexbox.
 var issFlexBox = document.body.querySelector('#issFlexBox');
 console.log(issFlexBox);
 
 // Fetch WhereIsTheISS API
 
-fetch('https://api.wheretheiss.at/v1/satellites', {
+fetch('https://api.wheretheiss.at/v1/satellites/25544/tles', {
         // method: "POST",
         // body: {}
     })
